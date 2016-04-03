c2036b22771ec937b6b8c48c4deb78df07ac9ea9
Tyler Green <tgreen8091@gmail.com>
2016-04-03 00:23:10 +0000

    Add JavaScript libraries for client Add Socket.io to client Create Angular controller in client Update server to send JSON across websocket

--- server.js
+++ server.js
@@ -2,13 +2,47 @@ var http = require('http');
 var path = require('path');
 
 var express = require('express');
+var socketio = require('socket.io');
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
 
 var router = express();
 var server = http.createServer(router);
+var io = socketio.listen(server);
+
+var sockets = [];
+
+var requestSettings = {
+  method: 'GET',
+  url: 'http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb',
+  encoding: null
+};
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
+});
+
+setInterval(function() {
+  console.log("Fetching API...");
+  request(requestSettings, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+      var date = new Date(feed.header.timestamp.low*1000);
+      var data = {
+        date: date.toString(),
+        entities: feed.entity
+      };
+      console.log(date.toString());
+      sockets.forEach(function(socket) {
+        socket.emit('update',data);
+      });
+    }
+  });
+}, 5000);
+
+io.on('connection', function (socket) {
+  sockets.push(socket);
 });
